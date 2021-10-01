942a7eb91771e9720528ce2514dd9aa07bfc27fa
jiaweioss <70185568+jiaweioss@users.noreply.github.com>
2021-10-01 17:41:34 +0800

    Update main.java

--- main.java
+++ main.java
@@ -14,7 +14,7 @@ public class main {
 
     public static void print(StringBuilder word, List<String> operator) {
         List<String> Token = Arrays.asList("if", "else", "break", "continue", "while", "return");
-        List<String> Tname = Arrays.asList("If", "Else", "While", "Break", "Continue", "Return");
+        List<String> Tname = Arrays.asList("If", "Else",  "Break", "Continue","While", "Return");
         List<String> Oname = Arrays.asList("Assign", "Semicolon", "LPar", "RPar", "LBrace", "RBrace", "Plus", "Mult", "Div", "Lt", "Gt", "Eq");
         if (operator.contains(word.toString())) {
             System.out.println(Oname.get(operator.indexOf(word.toString())));
@@ -42,6 +42,7 @@ public class main {
                     if (word.toString().equals("=")) {
                         word.append("=");
                         print(word, operator);
+                        word = new StringBuilder();
                     } else {
                         print(word, operator);
                         word = new StringBuilder("=");
@@ -53,6 +54,7 @@ public class main {
             } else if ((s.charAt(0) >= 97 && s.charAt(0) <= 122) || (s.charAt(0) >= 48 && s.charAt(0) <= 57) || (s.charAt(0) >= 65 && s.charAt(0) <= 90) || s.equals("_")) {
                 if (operator.contains(word.toString())) {
                     print(word, operator);
+                    word = new StringBuilder();
                 }
                 word.append(s);
             } else if (s.equals(" ") || s.equals("\n")) {
@@ -68,15 +70,14 @@ public class main {
     }
 
     public static void main(String[] args) throws FileNotFoundException {
+//        String pathname = "D:\\课程\\Java lab\\词法分析\\test.txt";
         String pathname = args[0];
         Scanner sc = new Scanner(new File(pathname));
         int i = 0;
-        while (sc.hasNextLine())//逐行读取文件内容
+        while (sc.hasNextLine()&&i == 0)//逐行读取文件内容
         {
             String line = sc.nextLine();
-            while (i == 0) {
-                i = analyze(line + " ");
-            }
+            i = analyze(line + " ");
 
             //System.out.println(line);
         }
