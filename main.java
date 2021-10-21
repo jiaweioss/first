94a45414ee95e92a7f6e9d080649e07c76773754
jiaweioss <70185568+jiaweioss@users.noreply.github.com>
2021-10-21 13:28:10 +0800

    Update main.java

--- main.java
+++ main.java
@@ -10,9 +10,10 @@ public class main {
 
 
     public static void main(String[] args) throws IOException {
-//         String pathname = ".\\test.txt";
+//        String pathname = ".\\test.txt";
         String pathname = args[0];
         BufferedReader bf = new BufferedReader(new FileReader(pathname));
+        File ir = new File(args[1]);
         StringBuilder tx = new StringBuilder();
         String line = "";
         int i = 0;
@@ -24,7 +25,7 @@ public class main {
             line += '\n';
             tx.append(line);
         }
-            if(lexcical.analyze(tx.toString())!=0){
+            if(lexcical.analyze(tx.toString(),ir)!=0){
                 throw new NullPointerException();
             }
     }
