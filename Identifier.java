f60c9ef69697ccd260f4f0847f0b18f1b118d034
jiawei <70185568+jiaweioss@users.noreply.github.com>
2021-11-20 23:12:41 +0800

    Update Identifier.java

--- Identifier.java
+++ Identifier.java
@@ -2,7 +2,9 @@ public class Identifier {
     String name;
     Integer value;
     IdentType type;
+    Integer globle;
     public Identifier(Integer value, String name,IdentType type){
+        this.globle = 0;
         this.value = value;
         this.name = name;
         this.type = type;
