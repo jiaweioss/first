caa56f8f1a07679723f35cc395edb1161ada1c26
jiawei <70185568+jiaweioss@users.noreply.github.com>
2021-11-25 10:12:46 +0800

    Update TargetCodeGenerator.java

--- TargetCodeGenerator.java
+++ TargetCodeGenerator.java
@@ -776,7 +776,7 @@ public class TargetCodeGenerator {
         } else if (list.size() > 1 && list.get(1).getToken().getValue().equals("(")) {
 
             func f = funcMap.getfuncMap().get(list.get(0).getNodeList().get(0).getToken().getValue());
-            if (Node.getNodeList().size() == 4) {
+            if (list.size() == 4) {
                 if (f.type.equals("int")) {
 
                     String s = printFuncRExp(List.get(List.size() - 2), blockID, f);
