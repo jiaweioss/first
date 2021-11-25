c7666c7d8c8cee92d1ecf9785501c2e968483aa9
jiawei <70185568+jiaweioss@users.noreply.github.com>
2021-11-25 10:44:54 +0800

    Update funcMap.java

--- funcMap.java
+++ funcMap.java
@@ -3,10 +3,16 @@ import java.util.HashMap;
 public class funcMap {
     private final static HashMap<String,func> funcs = new HashMap<>();
 
+    private final static HashMap<String,func> IRfuncs = new HashMap<>();
+
     private funcMap(){
     }
 
     public static HashMap<String,func> getfuncMap(){
         return funcs;
+    }
+
+    public static HashMap<String,func> getIRfuncMap(){
+        return IRfuncs;
     }
 }
